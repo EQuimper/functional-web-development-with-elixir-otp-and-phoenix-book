1fa6479a4028de34fad269e83eb5c04c176d42c7
Emanuel Quimper <quimperemanuel@gmail.com>
2018-03-02 18:46:32 -0500

    Finish creation of all the entities

--- islands_engine/lib/islands_engine/guesses.ex
+++ islands_engine/lib/islands_engine/guesses.ex
@@ -1,9 +1,14 @@
 defmodule IslandsEngine.Guesses do
-  alias __MODULE__
+  alias IslandsEngine.{Coordinate, Guesses}
 
   @enforce_keys [:hits, :misses]
   defstruct [:hits, :misses]
 
   def new(), do:
     %Guesses{hits: MapSet.new(), misses: MapSet.new()}
+
+  def add(%Guesses{} = guesses, :hit, %Coordinate{} = coordinate), do:
+    update_in(guesses.hits, &MapSet.put(&1, coordinate))
+  def add(%Guesses{} = guesses, :miss, %Coordinate{} = coordinate), do:
+    update_in(guesses.misses, &MapSet.put(&1, coordinate))
 end
--- islands_engine/lib/islands_engine/islands.ex
+++ islands_engine/lib/islands_engine/islands.ex
@@ -13,6 +13,23 @@ defmodule IslandsEngine.Island do
     end
   end
 
+  def overlaps?(existing_island, new_island), do:
+    not MapSet.disjoint?(existing_island.coordinates, new_island.coordinates)
+
+  def guess(island, coordinate) do
+    case MapSet.member?(island.coordinates, coordinate) do
+      true ->
+        hit_coordinates = MapSet.put(island.hit_coordinates, coordinate)
+        {:hit, %{island | hit_coordinates: hit_coordinates}}
+      false -> :miss
+    end
+  end
+
+  def forested?(island), do:
+    MapSet.equal?(island.coordinates, island.hit_coordinates)
+
+  def types(), do: [:atoll, :dot, :l_shape, :s_shape, :square]
+
   defp offsets(:square), do: [{0, 0}, {0, 1}, {1, 0}, {1, 1}]
   defp offsets(:atoll), do: [{0, 0}, {0, 1}, {1, 1}, {2, 0}, {2, 1}]
   defp offsets(:dot), do: [{0, 0}]
